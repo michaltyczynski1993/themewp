2bd36a0ba6a790ece037b3ba6789cbea1892d0f3
Michal Tyczynski <m.tyczynski1993@gmail.com>
2023-09-04 17:28:01 +0200

    rwd optimalization

--- footer.php
+++ footer.php
@@ -1,4 +1,13 @@
-this is my footer
+<footer>
+        <?php wp_nav_menu(
+            array(
+                'theme_location' =>'footer',
+                'container' => '',
+                'menu_class' => 'footer-socials',
+            )
+        );?>
+    <div class="logo">Some logo here</div>
+</footer>
 <?php wp_footer();?>    
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -2,10 +2,12 @@
 
 function load_css(){
     wp_enqueue_style( 'style', get_stylesheet_uri() );
+    // navbar styles
     wp_enqueue_style( 'navbar', get_template_directory_uri() . '/css/navbar.css', false, '1.1', 'all');
     wp_enqueue_style('lato', get_template_directory_uri() . 'https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap', false, '1.1', 'all');
 
-
+    // footer styles
+    wp_enqueue_style('footer', get_template_directory_uri() . '/css/footer.css', false, '1.1', 'all');
 }
 add_action('wp_enqueue_scripts', 'load_css');
 
@@ -13,5 +15,6 @@ add_action('wp_enqueue_scripts', 'load_css');
 // load custom menu
 register_nav_menus( array(
     'primary-menu'   => __( 'Primary Menu', 'themewp' ),
+    'footer' => __( 'Footer Menu', 'themewp' ),
 ) );
 
